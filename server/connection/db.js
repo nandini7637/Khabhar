7d6bb41747b6b9b89855dc026ca8c5590d63f855
Nandini Agarwal <nia78831@gmail.com>
2022-03-01 23:22:06 +0530

    key

--- server/connection/db.js
+++ server/connection/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const Connection = async () => {
     try {
-        const URL = `mongodb://nandini7637:J9fJaG2qhGyvcBOW@inshort-clone-shard-00-00.5xvsg.mongodb.net:27017,inshort-clone-shard-00-01.5xvsg.mongodb.net:27017,inshort-clone-shard-00-02.5xvsg.mongodb.net:27017/inshort-clone?ssl=true&replicaSet=atlas-p31tcq-shard-0&authSource=admin&retryWrites=true&w=majority`;
+        const URL = `mongodb://nandini7637:@inshort-clone-shard-00-00.5xvsg.mongodb.net:27017,inshort-clone-shard-00-01.5xvsg.mongodb.net:27017,inshort-clone-shard-00-02.5xvsg.mongodb.net:27017/inshort-clone?ssl=true&replicaSet=atlas-p31tcq-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
         await mongoose.connect(URL, { useNewUrlParser: true })
         console.log('Database connected successfully');
